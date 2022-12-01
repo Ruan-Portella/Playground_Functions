757b985c6cca4567455fb223aea0d0e9a84ea5e9
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2022-12-01 11:08:55 -0300

    Initial commit

---
+++ src/challenges.js
@@ -0,0 +1,36 @@
+// Desafio 1 - Crie a função compareTrue
+
+// Desafio 2 - Crie a função splitSentence
+
+// Desafio 3 - Crie a função concatName
+
+// Desafio 4 - Crie a função footballPoints
+
+// Desafio 5 - Crie a função highestCount
+
+// Desafio 6 - Crie as funções calcTriangleArea, calcRectangleArea e calcAllAreas
+
+// Desafio 7 - Crie a função catAndMouse
+
+// Desafio 8 - Crie a função fizzBuzz
+
+// Desafio 9 - Crie a função encode e a função decode
+
+// Desafio 10 - Crie a função techList
+
+// Não modifique essas linhas
+module.exports = {
+  calcTriangleArea: typeof calcTriangleArea === 'function' ? calcTriangleArea : (() => {}),
+  calcRectangleArea: typeof calcRectangleArea === 'function' ? calcRectangleArea : (() => {}),
+  calcAllAreas: typeof calcAllAreas === 'function' ? calcAllAreas : (() => {}),
+  catAndMouse: typeof catAndMouse === 'function' ? catAndMouse : (() => {}),
+  compareTrue: typeof compareTrue === 'function' ? compareTrue : (() => {}),
+  concatName: typeof concatName === 'function' ? concatName : (() => {}),
+  decode: typeof decode === 'function' ? decode : (() => {}),
+  encode: typeof encode === 'function' ? encode : (() => {}),
+  fizzBuzz: typeof fizzBuzz === 'function' ? fizzBuzz : (() => {}),
+  footballPoints: typeof footballPoints === 'function' ? footballPoints : (() => {}),
+  highestCount: typeof highestCount === 'function' ? highestCount : (() => {}),
+  splitSentence: typeof splitSentence === 'function' ? splitSentence : (() => {}),
+  techList: typeof techList === 'function' ? techList : (() => {}),
+};
---
+++ src/challenges2.js
@@ -0,0 +1,14 @@
+// Desafio 11 - Crie a função generatePhoneNumber
+
+// Desafio 12 -  Crie a função triangleCheck
+
+// Desafio 13 - Crie a função hydrate
+
+/* eslint no-undef: 0 */
+
+// Não modifique essas linhas
+module.exports = {
+  generatePhoneNumber: typeof generatePhoneNumber === 'function' ? generatePhoneNumber : (() => {}),
+  triangleCheck: typeof triangleCheck === 'function' ? triangleCheck : (() => {}),
+  hydrate: typeof hydrate === 'function' ? hydrate : (() => {}),
+};
